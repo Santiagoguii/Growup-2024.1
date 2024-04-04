7784d65c84a521c2cd7266e5ddcb4c938f62d936
Dizeu <122738432+DizeuM@users.noreply.github.com>
2024-04-03 23:02:50 -0300

    return Hello World

---
+++ SistemaBiblioteca/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaBiblioteca.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloWorldController : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult HelloWorld()
+    {
+        return  Ok("Hello World");
+    }
+
+}
