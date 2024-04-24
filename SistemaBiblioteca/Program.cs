e76cbb265fac284d71211a2bfd474f4c390bbdac
Santiagoguii <gp3375097@gmail.com>
2024-04-24 19:11:04 -0300

    up

--- SistemaBiblioteca/Program.cs
+++ SistemaBiblioteca/Program.cs
@@ -3,28 +3,28 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen((c =>
+builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
     {
-        Title = "Biblioteca API",
+        Title = "Sistema biblioteca",
         Version = "v1"
     });
 });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI((c =>
+    app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Biblioteca API V1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Crud_usuarios API V1");
     });
+}
 
 app.UseHttpsRedirection();
 
