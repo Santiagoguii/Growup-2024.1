056722307fb1b369e915fc45151c81ecb5fad193
clarirodrigues <2005.clarice@gmail.com>
2024-04-25 16:55:02 -0300

    Criação da classe Livro e seu Controller

---
+++ SistemaBiblioteca/Controllers/LivrosController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SistemaBiblioteca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemaBiblioteca.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        private readonly ILogger<LivrosController> _logger;
+        private readonly List<Livro> _livros = new List<Livro>();
+
+        public LivrosController(ILogger<LivrosController> logger)
+        {
+            _logger = logger;
+        }
+
+        #region Listar livros
+        [HttpGet]
+        public IActionResult GetLivros()
+        {
+            return Ok(_livros);
+        }
+
+        // Obter um livro por Id
+        [HttpGet("{id}")]
+        public IActionResult GetLivroById(int id)
+        {
+            var livro = _livros.FirstOrDefault(l => l.Id == id);
+            if (livro == null)
+            {
+                return NotFound("Livro não encontrado.");
+            }
+            return Ok(livro);
+        }
+        #endregion
+
+        #region Criação de livro
+        [HttpPost]
+        public IActionResult CreateLivro([FromBody] Livro livro)
+        {
+            // Verifica se o livro já existe pelo Id
+            if (_livros.Any(l => l.Id == livro.Id))
+            {
+                return Conflict("Livro com o mesmo Id já existe.");
+            }
+
+            _livros.Add(livro);
+            return Ok($"Livro criado com sucesso! ID: {livro.Id}");
+        }
+        #endregion
+
+        #region Atualizar livro
+        [HttpPut("{id}")]
+        public IActionResult UpdateLivro(int id, Livro updatedLivro)
+        {
+            var livro = _livros.FirstOrDefault(l => l.Id == id);
+            if (livro == null)
+            {
+                return NotFound("Livro não encontrado.");
+            }
+            livro.Titulo = updatedLivro.Titulo;
+            livro.Autor = updatedLivro.Autor;
+            livro.Genero = updatedLivro.Genero;
+            return Ok("Informações do livro atualizadas com sucesso!");
+        }
+        #endregion
+
+        #region Deletar livro
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLivro(int id)
+        {
+            var livro = _livros.FirstOrDefault(l => l.Id == id);
+            if (livro == null)
+            {
+                return NotFound("Livro não encontrado.");
+            }
+            _livros.Remove(livro);
+            return Ok("Livro excluído com sucesso!");
+        }
+        #endregion
+    }
+}
---
+++ SistemaBiblioteca/Models/Livro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaBiblioteca.Models
+{
+    public class Livro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Genero { get; set; }
+    }
+}
